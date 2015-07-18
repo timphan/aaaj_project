882c71c47694948b0dfe183fa46f66656532bac3
Tim Phan <timphan@Timothys-MacBook-Pro-2.local>
2015-07-18 04:25:31 -0700

    helloggg

--- dota.r
+++ dota.r
@@ -3,4 +3,4 @@ brood<- html("http://www.dotabuff.com/players/119721201/matches?date=&hero=brood
 brood_table<- html("http://www.dotabuff.com/players/119721201/matches?date=&hero=broodmother") %>% html_table()
 brood %>% html_nodes (xpath= '//*[@id="page-content"]/section/article[2]/table/tbody/tr[1]/td[7]/div[1]/a/img')
 brood %>% html_nodes(xpath = '//*[@id="page-content"]/section/article[2]/table/tbody/tr[1]/td[7]/div/a/img') %>% html_attr("title")
-hi tim!
+hi tim! i think you understand github now
