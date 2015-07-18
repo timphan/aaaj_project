be24074111fa0a0719ea0f8dd78c855a97b83e65
Tim Phan <timphan@Timothys-MacBook-Pro-2.local>
2015-07-18 04:21:47 -0700

    dota

---
+++ dota.r
@@ -0,0 +1,5 @@
+library(rvest)
+brood<- html("http://www.dotabuff.com/players/119721201/matches?date=&hero=broodmother")
+brood_table<- html("http://www.dotabuff.com/players/119721201/matches?date=&hero=broodmother") %>% html_table()
+brood %>% html_nodes (xpath= '//*[@id="page-content"]/section/article[2]/table/tbody/tr[1]/td[7]/div[1]/a/img')
+brood %>% html_nodes(xpath = '//*[@id="page-content"]/section/article[2]/table/tbody/tr[1]/td[7]/div/a/img') %>% html_attr("title")
